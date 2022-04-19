4030849f921003524db972bde7f07536596c1283
Kajal Kumari <kajal.kumar@innobisystems.com>
2022-04-19 10:47:33 +0530

    design added

---
+++ app/src/main/java/com/kajal/videoplayer/FavouriteActivity.kt
@@ -0,0 +1,18 @@
+package com.kajal.videoplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kajal.videoplayer.databinding.ActivityFavouriteBinding
+
+class FavouriteActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityFavouriteBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.coolPink)
+
+        binding = ActivityFavouriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/kajal/videoplayer/MainActivity.kt
+++ app/src/main/java/com/kajal/videoplayer/MainActivity.kt
@@ -1,12 +1,70 @@
 package com.kajal.videoplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.kajal.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //klj
+        requestRuntimePermission()
+        setTheme(R.style.coolPinkNav)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.shuffleBtn.setOnClickListener{
+            startActivity(Intent(this@MainActivity,PlayerActivity::class.java))
+
+        }
+        binding.favrtBtn.setOnClickListener{
+
+
+            startActivity(Intent(this@MainActivity,FavouriteActivity::class.java))
+        }
+
+        binding.playlistBtn.setOnClickListener{
+
+
+            startActivity(Intent(this@MainActivity,playlistActivity::class.java))
+        }
+
+    }
+
+
+    //for req permission
+    private fun requestRuntimePermission(){
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        !=PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode, permissions, grantResults
+        )
+        if (requestCode == 13) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            else
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    13)
+
+        }
     }
 }
