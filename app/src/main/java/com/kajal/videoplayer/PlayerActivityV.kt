1ca83f8cb6f8176f902a30f620ff378fca891b22
chhama gupta <chhama.gupta@innobitsystems.com>
2022-05-18 17:39:08 +0530

    Added play,pause,back,next and previous button

--- app/src/main/java/com/kajal/videoplayer/PlayerActivityV.kt
+++ app/src/main/java/com/kajal/videoplayer/PlayerActivityV.kt
@@ -21,19 +21,33 @@ class PlayerActivityV : AppCompatActivity() {
         binding = ActivityPlayervBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initializeLayout()
+        initializeBinding()
     }
     private fun initializeLayout(){
         when(intent.getStringExtra("class")){
             "AllVideos" ->{
                 playerList = ArrayList()
                 playerList.addAll(MainActivity.videoList)
+                createPlayer()
             }
             "FoldersActivity" ->{
                 playerList = ArrayList()
                 playerList.addAll(FoldersActivity.currentFoldersVideo)
+                createPlayer()
             }
         }
-        createPlayer()
+
+    }
+    private fun initializeBinding(){
+        binding.backBtnPA.setOnClickListener {
+            finish()
+        }
+        binding.playpauseBtnPA.setOnClickListener {
+              if(player.isPlaying) pauseVideo()
+            else playVideo()
+        }
+        binding.nextBtnPA.setOnClickListener { nextPrevVideo() }
+        binding.previousBtnPA.setOnClickListener { nextPrevVideo(isNext = false) }
     }
     private fun createPlayer(){
         binding.videoTittle.text = playerList[position].title
@@ -44,9 +58,35 @@ class PlayerActivityV : AppCompatActivity() {
         player.setMediaItem(mediaItem)
         Log.d("mediaItemssss",mediaItem.toString())
         player.prepare()
-        player.play()
+        playVideo()
     }
 
+
+    private fun playVideo(){
+        binding.playpauseBtnPA.setImageResource(R.drawable.pause_icon)
+        player.play()
+    }
+    private fun pauseVideo(){
+        binding.playpauseBtnPA.setImageResource(R.drawable.play_icon)
+        player.pause()
+    }
+    private fun nextPrevVideo(isNext: Boolean = true){
+        player.release()
+        if(isNext) setPosition()
+        else setPosition(isIncrement = false)
+        createPlayer()
+    }
+    private fun setPosition(isIncrement: Boolean = true){
+        if(isIncrement){
+            if(playerList.size -1 == position)
+                position = 0
+            else position++
+        }else{
+            if(position  == 0)
+                position = playerList.size -1
+            else position--
+        }
+    }
     override fun onDestroy() {
         super.onDestroy()
         player.release()
